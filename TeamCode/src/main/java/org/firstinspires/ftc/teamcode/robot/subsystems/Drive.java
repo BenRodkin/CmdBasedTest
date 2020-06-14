b64c216820affd974c0ffc20022e9370db8d4a2b
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2020-06-13 23:24:42 -0400

    Added Drive subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Drive.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems;
+
+import com.disnodeteam.dogecommander.Subsystem;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Drive implements Subsystem {
+    // Hardware map
+    private HardwareMap hardwareMap;
+
+    // Components
+    private DcMotor flDrive;
+    private DcMotor frDrive;
+    private DcMotor rlDrive;
+    private DcMotor rrDrive;
+
+    // State and interface variables
+    private double flPower = 0;
+    private double frPower = 0;
+    private double rlPower = 0;
+    private double rrPower = 0;
+
+    // Constructor
+    public Drive(HardwareMap hardwareMap) {
+        this.hardwareMap = hardwareMap;
+    }
+
+    // Interface methods
+    public void setPower(double leftPower, double rightPower) {
+        flPower = leftPower;
+        frPower = leftPower;
+        rlPower = rightPower;
+        rrPower = rightPower;
+    }
+
+    @Override
+    public void initHardware() {
+        flDrive = hardwareMap.get(DcMotor.class, "fl_drive");
+        frDrive = hardwareMap.get(DcMotor.class, "fr_drive");
+        rlDrive = hardwareMap.get(DcMotor.class, "rl_drive");
+        rrDrive = hardwareMap.get(DcMotor.class, "rr_drive");
+
+        // Reverse left side
+        flDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rlDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        flDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rlDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rrDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+    }
+
+    @Override
+    public void periodic() {
+        flDrive.setPower(flPower);
+        frDrive.setPower(frPower);
+        rlDrive.setPower(rlPower);
+        rrDrive.setPower(rrPower);
+
+}
+
+
+
+
+
+
+}
