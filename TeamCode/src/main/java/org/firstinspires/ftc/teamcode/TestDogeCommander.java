9104b3969da541e51787220e1e9ecc4f3bffc130
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2020-06-17 00:44:27 -0400

    Added Gripper controls to main TeleOp class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDogeCommander.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDogeCommander.java
@@ -9,12 +9,14 @@ import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpArmControl;
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpCapstoneControl;
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpDriveControl;
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpFoundationControl;
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpGripperControl;
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpIntakeControl;
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpTapeDriveControl;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Capstone;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Drive;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Foundation;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Gripper;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.robot.subsystems.TapeDrive;
 
@@ -29,6 +31,7 @@ public class TestDogeCommander extends LinearOpMode implements DogeOpMode {
         TapeDrive tapeDrive     = new TapeDrive(hardwareMap);
         Capstone capstone       = new Capstone(hardwareMap);
         Arm arm                 = new Arm(hardwareMap);
+        Gripper gripper         = new Gripper(hardwareMap);
 
         commander.registerSubsystem(drive);
         commander.registerSubsystem(intake);
@@ -36,6 +39,7 @@ public class TestDogeCommander extends LinearOpMode implements DogeOpMode {
         commander.registerSubsystem(tapeDrive);
         commander.registerSubsystem(capstone);
         commander.registerSubsystem(arm);
+        commander.registerSubsystem(gripper);
         commander.init();
 
         waitForStart();
@@ -43,10 +47,11 @@ public class TestDogeCommander extends LinearOpMode implements DogeOpMode {
         commander.runCommandsParallel(
                 new TeleOpDriveControl(drive, gamepad1),
                 new TeleOpIntakeControl(intake, gamepad2),
-                new TeleOpFoundationControl(foundation, gamepad2),
+                new TeleOpFoundationControl(foundation, gamepad1),
                 new TeleOpTapeDriveControl(tapeDrive, gamepad2),
                 new TeleOpCapstoneControl(capstone, gamepad2),
                 new TeleOpArmControl(arm, gamepad2),
+                new TeleOpGripperControl(gripper, gamepad2)
         );
 
 
