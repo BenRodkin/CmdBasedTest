cc4cc64667f5ea8b84ef1a26c9f5daa10a75de48
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2020-06-26 23:28:13 -0400

    Added methods to Drive Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Drive.java
@@ -1,10 +1,16 @@
 package org.firstinspires.ftc.teamcode.robot.subsystems;
 
 import com.disnodeteam.dogecommander.Subsystem;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
 public class Drive implements Subsystem {
     // Hardware map
@@ -18,6 +24,7 @@ public class Drive implements Subsystem {
     private DcMotor frDrive;
     private DcMotor rlDrive;
     private DcMotor rrDrive;
+    private BNO055IMU imu;
 
     // State and interface variables
     private double flPower = 0;
@@ -25,30 +32,18 @@ public class Drive implements Subsystem {
     private double rlPower = 0;
     private double rrPower = 0;
     private double goSlow = FAST;
+    private boolean initIMU;
 
 
 
 
     // Constructor
-    public Drive(HardwareMap hardwareMap) {
+    public Drive(HardwareMap hardwareMap, boolean initIMU) {
         this.hardwareMap = hardwareMap;
+        this.initIMU = initIMU;
     }
 
-    // Interface methods
-    public void setPower(double leftPower, double rightPower) {
-        this.flPower = leftPower;
-        this.frPower = rightPower;
-        this.rlPower = leftPower;
-        this.rrPower = rightPower;
-    }
 
-    public void setMecanumPower(double drive, double strafe, double twist, boolean goSlow) {
-        this.goSlow = goSlow ? SLOW : FAST;
-        flPower = (drive + strafe + twist) * this.goSlow;
-        frPower = (drive - strafe - twist) * this.goSlow;
-        rlPower = (drive - strafe + twist) * this.goSlow;
-        rrPower = (drive + strafe - twist) * this.goSlow;
-    }
 
     @Override
     public void initHardware() {
@@ -56,6 +51,19 @@ public class Drive implements Subsystem {
         frDrive = hardwareMap.get(DcMotor.class, "fr_drive");
         rlDrive = hardwareMap.get(DcMotor.class, "rl_drive");
         rrDrive = hardwareMap.get(DcMotor.class, "rr_drive");
+
+        if(initIMU){
+            BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+            parameters.angleUnit            = BNO055IMU.AngleUnit.DEGREES;
+            parameters.accelUnit            = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+            parameters.calibrationDataFile  = "BNO055IMUCalibration.json";
+            parameters.loggingEnabled       = true;
+            parameters.loggingTag           = "IMU";
+            parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+            imu = hardwareMap.get(BNO055IMU.class, "imu");
+            imu.initialize(parameters);
+        }
 
         // Reverse left side
         flDrive.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -76,7 +84,56 @@ public class Drive implements Subsystem {
         rlDrive.setPower(rlPower);
         rrDrive.setPower(rrPower);
 
-}
+    }
+    // Interface methods
+    public void setPower(double leftPower, double rightPower) {
+        this.flPower = leftPower;
+        this.frPower = rightPower;
+        this.rlPower = leftPower;
+        this.rrPower = rightPower;
+    }
+
+    public void setMecanumPower(double drive, double strafe, double twist, boolean goSlow) {
+        this.goSlow = goSlow ? SLOW : FAST;
+        flPower = (drive + strafe + twist) * this.goSlow;
+        frPower = (drive - strafe - twist) * this.goSlow;
+        rlPower = (drive - strafe + twist) * this.goSlow;
+        rrPower = (drive + strafe - twist) * this.goSlow;
+    }
+
+    public void setTargets(int flTarget, int frTarget, int rlTarget, int rrTarget) {
+        flDrive.setTargetPosition(flTarget);
+        frDrive.setTargetPosition(frTarget);
+        rlDrive.setTargetPosition(rlTarget);
+        rrDrive.setTargetPosition(rrTarget);
+    }
+
+    public void setRunMode(DcMotor.RunMode runMode){
+        flDrive.setMode(runMode);
+        frDrive.setMode(runMode);
+        rlDrive.setMode(runMode);
+        rrDrive.setMode(runMode);
+    }
+
+    public int[] getCurrentPositions(){
+        return new int[]{flDrive.getCurrentPosition(), frDrive.getCurrentPosition(), rlDrive.getCurrentPosition(), rrDrive.getCurrentPosition()};
+    }
+
+    public DcMotor.RunMode getRunMode(){
+        return flDrive.getMode();
+    }
+
+    public boolean driveIsBusy(){
+        return flDrive.isBusy() || frDrive.isBusy() || rlDrive.isBusy() || rrDrive.isBusy();
+    }
+
+    public float heading(){
+        return imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+    }
+
+
+
+
 
 
 
